63cdc057a92617701096a909b7c3cf1db59707ee
Restiii <restisanchez99@gmail.com>
2021-06-02 20:39:28 +0200

    Update back.sh

--- Fase_1/back.sh
+++ Fase_1/back.sh
@@ -8,13 +8,16 @@ DB_PASSWORD=wordpress_password
 IP_PRIVADA_FRONTEND=
 IP_PRIVADA_BACK=
 
-#Actualizamos
+# Actualizamos
+
 apt update
 
-#Instalamos MySQL Server
+# Instalamos MySQL Server
+
 apt install mysql-server -y
 
-#Creamos la base de datos para wordpress
+# Creamos la base de datos para wordpress
+
 mysql -u root <<< "DROP DATABASE IF EXISTS $DB_NAME;"
 mysql -u root <<< "CREATE DATABASE $DB_NAME;"
 mysql -u root <<< "CREATE USER $DB_USER@'$IP_PRIVADA_FRONTEND' IDENTIFIED BY '$DB_PASSWORD';"
@@ -22,7 +25,10 @@ mysql -u root <<< "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'$IP_PRIVADA_F
 mysql -u root <<< "FLUSH PRIVILEGES;"
 
 # Modificamos el valor de bind-address para permitir conexiones remotas
+
 sed -i "s/127.0.0.1/$IP_PRIVADA_BACK/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
+
 # Reiniciamos mysql
+
 systemctl restart mysql
